8662f2357837fe39602c44c30871658869191218
Axel Heider <axelheider@gmx.de>
2022-02-15 04:58:34 +0100

    repo-checkout: check repo tool version
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- repo-checkout/steps.sh
+++ repo-checkout/steps.sh
@@ -11,6 +11,8 @@ echo "::group::Setting up"
 
 mkdir -p ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+# check we got repo V1.21
+echo "72fc70040dad13f6f9e9d0de82beb98aff41fde0c9ed810c9251cb9500d3c8c0 $(readlink -f ~)/bin/repo" | sha256sum -c -
 chmod a+x ~/bin/repo
 PATH=~/bin:$PATH
 
