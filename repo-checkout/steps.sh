ddb8abb9da7405d4aa25c7ed1bb38985d08d37e6
Axel Heider <axelheider@gmx.de>
2024-01-05 10:50:10 +0100

    docker: add docker image for repo checkout
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- repo-checkout/steps.sh
+++ repo-checkout/steps.sh
@@ -7,19 +7,6 @@
 
 set -e
 
-echo "::group::Setting up"
-
-mkdir -p ~/bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-# check we got repo V1.21
-echo "72fc70040dad13f6f9e9d0de82beb98aff41fde0c9ed810c9251cb9500d3c8c0 $(readlink -f ~)/bin/repo" | sha256sum -c -
-chmod a+x ~/bin/repo
-PATH=~/bin:$PATH
-
-pip3 install -U PyGithub
-
-echo "::endgroup::"
-
 echo "::group::Repo checkout"
 
 export REPO_MANIFEST="${INPUT_MANIFEST}"
